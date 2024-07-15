811d0cfe003e9e04a9a3e79ed730b8c9f6858351
deivisonmaster <deivisonoliveira.info@gmail.com>
2024-07-15 14:12:54 -0400

    criação dos serviços Usuario e Carteira

---
+++ src/main/java/br/com/picpay/service/CarteiraServico.java
@@ -0,0 +1,26 @@
+package br.com.picpay.service;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.picpay.model.Carteira;
+import br.com.picpay.model.Usuario;
+import br.com.picpay.repository.CarteiraRepositorio;
+
+@Service
+public class CarteiraServico {
+
+	@Autowired
+	private CarteiraRepositorio carteiraRepositorio;
+	
+	public void novaCarteira(Usuario usuarioSalvo) {
+		Carteira carteira = new Carteira();
+		carteira.setSaldo(BigDecimal.ZERO);
+		carteira.setUsuario(usuarioSalvo);
+
+		carteiraRepositorio.save(carteira);
+	}
+
+}
---
+++ src/main/java/br/com/picpay/service/UsuarioServico.java
@@ -0,0 +1,46 @@
+package br.com.picpay.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.picpay.exception.DadosUnicosExistentesExcecao;
+import br.com.picpay.model.TipoUsuario;
+import br.com.picpay.model.Usuario;
+import br.com.picpay.model.dto.UsuarioDTO;
+import br.com.picpay.repository.UsuarioRepositorio;
+
+@Service
+public class UsuarioServico {
+	
+	@Autowired
+	private UsuarioRepositorio usuarioRepositorio;
+	
+	@Autowired
+	private CarteiraServico carteiraServico;
+
+	public Usuario novo(UsuarioDTO usuarioDTO) {
+		Optional<Usuario> usuarioPesquisa = usuarioRepositorio.findByCpfOrCnpjOrEmail(usuarioDTO.cpf(), usuarioDTO.cnpj(), usuarioDTO.email());
+		if(usuarioPesquisa.isPresent()) {
+			throw new DadosUnicosExistentesExcecao("CPF/CNPJ ou Email já estão cadastrados no sistema!");
+		}
+		
+		Usuario usuario = new Usuario();
+		usuario.setNome(usuarioDTO.nome());
+		usuario.setSobreNome(usuarioDTO.sobreNome());
+		usuario.setCpf(usuarioDTO.cpf());
+		usuario.setCnpj(usuarioDTO.cnpj());
+		usuario.setTipoUsuario(usuarioDTO.tipoUsuario().toUpperCase().equals(TipoUsuario.COMUM.toString()) ? TipoUsuario.COMUM : TipoUsuario.LOJISTA);
+		usuario.setEmail(usuarioDTO.email());
+		usuario.setSenha(usuarioDTO.senha());
+		
+		Usuario usuarioSalvo = usuarioRepositorio.save(usuario);
+		
+		carteiraServico.novaCarteira(usuarioSalvo);
+		
+		return usuarioSalvo;
+	}
+	
+	
+}
